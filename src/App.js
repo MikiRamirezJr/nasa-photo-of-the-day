d71e6ed04b21e61595f288bac349f22dffb2a4a1
Miguel Ramirez <miki1752@gmail.com>
2023-03-28 18:22:57 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
 import "./App.css";
 
+import NasaPhoto from "./Components/NasaPhoto";
+
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+   const [data, setData] = useState();
+
+   useEffect(() => {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+          setData(res.data);
+      })
+      .catch(err => console.log(err))
+      }, [])
+
+    return (
+      <div className="App">
+        { data && <NasaPhoto photo={data} /> }
+      </div>
+    );
 }
 
 export default App;
+/** 
+date: "2023-03-25"
+explanation: 
+"On March 23 early evening skygazers could watch Venus and a young crescent moon, both near the western horizon. On that date Earth's brilliant evening star, faint lunar night side and slender sunlit crescent were captured in this telephoto skyscape posing alongside a church tower from Danta di Cadore, Dolomiti, Italy. Of course the subtle lunar illumination is earthshine, earthlight reflected from the Moon's night side. A description of earthshine, in terms of sunlight reflected by Earth's oceans illuminating the Moon's dark surface, was written over 500 years ago by Leonardo da Vinci. On March 24, from some locations the Moon could be seen to occult or pass in front of Venus. Around the planet tonight, a waxing lunar crescent will appear near the Pleiades star cluster."
+hdurl: "https://apod.nasa.gov/apod/image/2303/_GHR3094-venerelunafirma.jpg"
+title: "Venus and the Da Vinci Glow"
+url: "https://apod.nasa.gov/apod/image/2303/_GHR3094-venerelunafirma800.jpg"
+ */
---
+++ src/Components/NasaPhoto.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const NasaPhoto = (props) => {
+    return (
+       <div className="nasa-photo-wrapper">
+        <h1>{props.photo.title}</h1>
+        <p>{props.photo.date}</p>
+        <img src={props.photo.hdurl} alt=""/>
+        <p className="explanation">{props.photo.explanation}</p>
+        </div>
+    )
+}
+
+export default NasaPhoto;
